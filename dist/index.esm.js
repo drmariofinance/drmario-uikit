f5d7036508c2dc591c39898a194b77e67750a225
drmariofinance <Dr.Mario_Finance@protonmail.com>
2021-03-07 23:10:02 +0800

    update color

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2763,7 +2763,7 @@ var baseColors = {
     failure: "#ED4B9E",
     primary: "#2683c3",
     primaryBright: "#2683c3",
-    primaryDark: "#f16665",
+    primaryDark: "#df3f3e",
     secondary: "#2883c3",
     success: "#31D0AA",
     warning: "#FFB237",
@@ -2774,7 +2774,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#2683c3", textDisabled: "#BDC2C4", textSubtle: "#2683c3", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#f16665", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#f16665", textDisabled: "#666171", textSubtle: "#f16665", borderColor: "#524B63", card: "#27262c", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#df3f3e", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#df3f3e", textDisabled: "#666171", textSubtle: "#df3f3e", borderColor: "#524B63", card: "#27262c", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
